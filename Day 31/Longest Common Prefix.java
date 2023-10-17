ef2ffbb4a00c8a81ba2639415e301e1bfc9bdb84
ritikgg <48135557+ritikgg@users.noreply.github.com>
2023-10-18 01:58:31 +0530

    Create Longest Common Prefix.java

---
+++ Day 31/Longest Common Prefix.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        
+        int minLength = Integer.MAX_VALUE;
+        for (String str : strs) {
+            if (str.length() < minLength) {
+                minLength = str.length();
+            }
+        }
+        
+        StringBuilder commonPrefix = new StringBuilder();
+        
+        for (int i = 0; i < minLength; i++) {
+            char currentChar = strs[0].charAt(i);
+            
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].charAt(i) != currentChar) {
+                    return commonPrefix.toString();
+                }
+            }
+            
+            commonPrefix.append(currentChar);
+        }
+        
+        return commonPrefix.toString();
+    }
+}
