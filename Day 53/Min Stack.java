5f1d564e437dc0703349f079cae29d2546062dc3
ritikgg <48135557+ritikgg@users.noreply.github.com>
2023-11-10 00:22:06 +0530

    Create Min Stack.java

---
+++ Day 53/Min Stack.java
@@ -0,0 +1,40 @@
+class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (!stack.isEmpty()) {
+            if (stack.peek().equals(minStack.peek())) {
+                minStack.pop();
+            }
+            stack.pop();
+        }
+    }
+
+    public int top() {
+        if (!stack.isEmpty()) {
+            return stack.peek();
+        }
+        throw new IllegalStateException("Stack is empty");
+    }
+
+    public int getMin() {
+        if (!minStack.isEmpty()) {
+            return minStack.peek();
+        }
+        throw new IllegalStateException("Stack is empty");
+    }
+}
