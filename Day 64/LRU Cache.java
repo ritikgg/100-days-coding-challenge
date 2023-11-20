06629e6aed935be8f12faa97627f90f40cc4c3c4
ritikgg <48135557+ritikgg@users.noreply.github.com>
2023-11-21 02:19:29 +0530

    Create LRU Cache.java

---
+++ Day 64/LRU Cache.java
@@ -0,0 +1,30 @@
+class LRUCache {
+    private final int capacity;
+    private final Map<Integer, Integer> cache;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new LinkedHashMap<>(capacity,0.75f,true);        
+    }
+    
+    public int get(int key) {
+        return cache.getOrDefault(key,-1);
+    }
+    
+    public void put(int key, int value) {
+        cache.put(key,value);
+        if(cache.size() > capacity){
+            Iterator<Map.Entry<Integer, Integer>> iterator = cache.entrySet().iterator();
+            iterator.next();
+            iterator.remove();
+        }
+        
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
