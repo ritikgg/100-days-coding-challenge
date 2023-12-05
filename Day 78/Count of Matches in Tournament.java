9ff8eabfbf0776fb316b89a2fa9bfda63041e914
ritikgg <48135557+ritikgg@users.noreply.github.com>
2023-12-06 01:41:06 +0530

    Create Count of Matches in Tournament.java

---
+++ Day 78/Count of Matches in Tournament.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        return n-1;
+    }
+}
+//as simple as that
+//Problem link - https://leetcode.com/problems/count-of-matches-in-tournament/?envType=daily-question&envId=2023-12-05
