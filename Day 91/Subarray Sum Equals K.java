92bb17211121a26329aebb9f702010792c4bf57b
ritikgg <48135557+ritikgg@users.noreply.github.com>
2023-12-18 00:23:44 +0530

    Create Subarray Sum Equals K.java

---
+++ Day 91/Subarray Sum Equals K.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int sum = 0, result = 0;
+        map.put(sum, 1);
+
+        for (int n : nums) {
+            sum += n;
+            result += map.getOrDefault(sum - k, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return result;
+    }
+}
