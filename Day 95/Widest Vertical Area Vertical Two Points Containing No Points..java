9053d536eba179456a2b34ec29c3d59860d3db7e
ritikgg <48135557+ritikgg@users.noreply.github.com>
2023-12-22 03:57:03 +0530

    Create Widest Vertical Area Vertical Two Points Containing No Points..java

---
+++ Day 95/Widest Vertical Area Vertical Two Points Containing No Points..java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int res = 0;
+        for (int i = 1; i < points.length; i++) {
+            res = Math.max(res, points[i][0] - points[i - 1][0]);
+        }
+
+        return res;
+    }
+}
